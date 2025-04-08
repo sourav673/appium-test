4e1d4a3f07883433360dff08306a98236c1b18bd
Milind Deore <milind.deore@privittytech.com>
2025-04-08 12:12:27 +0530

    [DC] chat.delta.privitty & invite link
    This commit fix following:
    1. Changed package name from `chat.delta` to `chat.delta.privitty`
    2. Invite link URl modified in the app.
    
    Also, adding the dc-core shared objects (.so) with invite link
    modification.
    
    Bug: #6 #26
    Test:
    Tested and verified on real phone and simulator.

--- src/main/java/org/thoughtcrime/securesms/util/Util.java
+++ src/main/java/org/thoughtcrime/securesms/util/Util.java
@@ -64,7 +64,7 @@ import java.util.concurrent.CountDownLatch;
 
 public class Util {
   private static final String TAG = Util.class.getSimpleName();
-  private static final String INVITE_DOMAIN = "i.delta.chat";
+  private static final String INVITE_DOMAIN = "i.privittytech.com";
 
   public static final Handler handler = new Handler(Looper.getMainLooper());
 
